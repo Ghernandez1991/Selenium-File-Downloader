15479e611f660ba56a3d29c46a46e14af6ecafbe
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2023-05-25 20:35:05 -0500

    made functional and updated requirements.txt

--- App.py
+++ App.py
@@ -1,24 +1,27 @@
 # inmport dependencies
 from splinter import Browser
 from selenium import webdriver
-import bs4 as BS
-import time
 
-# store executible path for chrome driver.
-executable_path = {
-    "executable_path": r"C:\Users\Gabriel Hernandez\Desktop\MAME_DOWNLOADER\chromedriver.exe"}
-# create a browers object, we are not running headless because we want to see the files being downloaded.
-browser = Browser("chrome", **executable_path, headless=False)
+def main():
+    # store executible path for chrome driver.
+    executable_path = {
+        "executable_path": r"yourpathtoexecutable"}
+    # create a browers object, we are not running headless because we want to see the files being downloaded.
+    browser = Browser("chrome", **executable_path, headless=False)
 
-# create URL object
-url = 'https://archive.org/download/mame_sl_0220/MAME%20SL%200.220%20%28Merged%29/'
+    # create URL object
+    url = 'https://archive.org/download/mame_sl_0220/MAME%20SL%200.220%20%28Merged%29/'
 
-# tell browser to visit the website
-browser.visit(url)
+    # tell browser to visit the website
+    browser.visit(url)
 
-# loop though all the links. We start at table row two, and go to table row 566.  THis was found using chrome developer tools, and looking for the xpath of each link inside the html table.
-for row in range(2, 566):
-    browser.find_by_xpath(
-        f'//*[@id="maincontent"]/div/div/pre/table/tbody/tr{[row]}/td[1]/a[1]')[0].click()
-    # LET THE USER KNOW THE FILE HAS BEEN CLICKED.
-    print(f'link number{row} successfully clicked')
+    # loop though all the links. We start at table row two, and go to table row 566.  THis was found using chrome developer tools, and looking for the xpath of each link inside the html table.
+    for row in range(2, 566):
+        browser.find_by_xpath(
+            f'//*[@id="maincontent"]/div/div/pre/table/tbody/tr{[row]}/td[1]/a[1]')[0].click()
+        # LET THE USER KNOW THE FILE HAS BEEN CLICKED.
+        print(f'link number{row} successfully clicked')
+
+
+if __name__ == '__main__':
+    main()
